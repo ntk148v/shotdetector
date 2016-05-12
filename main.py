176d829456436aae23f65091e2b1b5719ca23105
Dai Dang Van <daikk115@gmail.com>
2016-05-13 00:43:42 +0700

    create simple GUI

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from shot_detector import ShotBoundaryDetector
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/start", methods=['POST'])
+def start():
+    detector = ShotBoundaryDetector(request.form['url'])
+    detector.detect()
+    return "Done!!"
+
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    detector = ShotBoundaryDetector('test.avi')
-    detector.detect()
+    app.debug = True
+    app.run(debug = True)
+
+# if __name__ == '__main__':
+#     detector = ShotBoundaryDetector('test.avi')
+#     detector.detect()
