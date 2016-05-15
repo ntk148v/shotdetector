66b4869acb895a4940cdc7652e7482940b63de15
Dai Dang Van <daikk115@gmail.com>
2016-05-15 22:27:37 +0700

    delete all file of master to merge gui

--- README.md
+++
@@ -1,5 +0,0 @@
-# shotdetector
-
-- OpenCV 
-- ShotDetector 
-- MMDB
--- main.py
+++
@@ -1,5 +0,0 @@
-from shot_detector import ShotBoundaryDetector
-
-if __name__ == '__main__':
-    detector = ShotBoundaryDetector('test.avi')
-    detector.detect()
